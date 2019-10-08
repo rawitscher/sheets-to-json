18c40c0be9d26c3cf3f05b085c784ce085295987
rawitscher <lindsayrawitscher2017@u.northwestern.edu>
2019-10-08 12:13:24 -0700

    auth restricted to specific pages

--- app.js
+++ app.js
@@ -1,34 +1,33 @@
-
 var api = require('./api');
-
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
-var basicAuth = require('express-basic-auth');
 
+// Create authentication module
+var dotenv = require('dotenv');
+var basicAuth = require('express-basic-auth');
+var basicAuthCall = basicAuth({
+                      authorizer: myAuthorizer,
+                      challenge: true,
+                      realm: "the site",
+                      unauthorizedResponse: function(req) {
+                        return req.auth
+                            ? ('Credentials ' + req.auth.user + ':' + req.auth.password + ' rejected')
+                            : 'No credentials provided'
+                          }
+                  });
+function myAuthorizer(username, password) {
+    const userMatches = basicAuth.safeCompare(username, process.env.USER_NAME);
+    const passwordMatches = basicAuth.safeCompare(password, process.env.USER_PASSW);
+ 
+    return userMatches & passwordMatches;
+};
+dotenv.config();
+
+// Create app
 var app = express();
 var server = require('http').createServer(app);
-
 var port = process.env.PORT || 5000;
-
-// force user login before page access is granted
-app.use(basicAuth({
-    authorizer: myAuthorizer,
-    challenge: true,
-    realm: "the site",
-    unauthorizedResponse: function(req) {
-      return req.auth
-          ? ('Credentials ' + req.auth.user + ':' + req.auth.password + ' rejected')
-          : 'No credentials provided'
-        }
-}))
-
-function myAuthorizer(username, password) {
-    const userMatches = basicAuth.safeCompare(username, process.env.USER_NAME)
-    const passwordMatches = basicAuth.safeCompare(password, process.env.USER_PASSW)
- 
-    return userMatches & passwordMatches
-}
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -38,21 +37,21 @@ app.use(function(req, res, next) {
   next();
 });
 
-// run static middleware 
-app.use(express.static(__dirname + '/static'));
-
-// body parser middleware
+// Body parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// get api
+// Get api
 app.get('/api', api);
-app.get('/',function(req,res) {
+
+// Run static middleware & load site
+app.use(basicAuthCall);
+app.use(express.static(__dirname + '/static'));
+app.get('/', function(req,res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-
-// error handler
+// Error handler
 app.use(function(err, req, res, next) {
   console.error(err.stack);
   res.status(400).send(err.message);
